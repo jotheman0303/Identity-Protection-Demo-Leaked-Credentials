a583c9b5e8206b41215e9623e335e56058388e06
Jop Gommans <34010893+jotheman0303@users.noreply.github.com>
2022-08-31 13:56:40 +0200

    Create LeakedCredentials-TopSecret-BLOCK.go

---
+++ LeakedCredentials-TopSecret-BLOCK.go
@@ -0,0 +1,6 @@
+
+# These are secret leaked credentials taken from the internet
+"AadClientId": "65deccd7-8522-468a-9c8b-7d05f7ac746f",
+"AadSecret": "H568Q~Xp4OWUSGrWbq3ZxI2cwVLathIDbYTIwcbl",
+"AadTenantDomain": "nedscaperlab.onmicrosoft.com",
+"AadTenantId": "316bec54-8d14-4882-8af3-ee5e61570603"
